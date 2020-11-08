04e31a92aaebd1106af04c81cf23d60f622181e7
kookminju <rlaalswn3282@kookmin.ac.kr>
2020-11-08 17:59:29 +0900

    shoporter-web-1 [컴포넌트 개발] 총 매출액 컴포넌트 구현

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
   <div id="app">
-    <bottomTabBar />
+    <totalMoney :callItem="1239" :totalSell="3123"/>
+    <bottomTabBar/>
   </div>
 </template>
 
 <script>
 import bottomTabBar from './components/bottomTabBar.vue'
-
+import totalMoney from "./components/totalMoney.vue";
 
 export default {
   name: 'App',
   components: {
     bottomTabBar,
+    totalMoney,
   }
 }
 </script>
 
 <style>
-  * {
-    margin: 0;
-    padding: 0;
-  }
-  body {
-    background-color: #EDF1F5;
-  }
+@font-face{ font-family: SpoqaHanSans; src: url(https://spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css) }
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: #EDF1F5;
+}
 </style>
