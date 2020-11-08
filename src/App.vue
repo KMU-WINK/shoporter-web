763aa2bf1a8c51d2dcc9c804c7d1687025a2d1b2
sunwoopia <sunwoopia@kookmin.ac.kr>
2020-11-08 23:49:57 +0900

    shoporter-web-3 [컴포넌트 개발] 로그인 페이지 컴포넌트 구현

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,37 @@
 <template>
   <div id="app">
-    <bottomTabBar />
+    <bottomTabBar/>
+    <login picture_src= static/assets/coupangLogo.png />
+    <Test :test3="test3"/>
   </div>
 </template>
 
 <script>
 import bottomTabBar from './components/bottomTabBar.vue'
-
-
+import Test from './components/Test';
+import login from './components/login';
 export default {
   name: 'App',
   components: {
     bottomTabBar,
+    Test,
+    login,
+  },
+  data(){
+    return{
+      test3:"asdf"
+    }
   }
 }
 </script>
 
 <style>
-  * {
-    margin: 0;
-    padding: 0;
-  }
-  body {
-    background-color: #EDF1F5;
-  }
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: #EDF1F5;
+}
 </style>
---
+++ src/components/Test.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p>{{ data }}</p>
+    <p>{{ data2 }}</p>
+    <p>{{ test3 }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {
+      data: "si",
+      data2: "bal"
+    }
+  },
+  props: {
+    test3: String
+  },
+  mounted() {
+    this.test2();
+  },
+  methods: {
+    //lexical scope vs dynamic scope
+    test2: function () {
+      this.test3 = "qwer";
+    },
+    test4: () => {
+
+    },
+    test1() {
+
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
